70b981583a8347b3ad23b0f21892474039e27328
Alyssa Hursh <akhursh@gmail.com>
2019-11-24 11:56:05 -0800

    Generate amplify API

---
+++ src/graphql/mutations.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createUser = `mutation CreateUser($input: CreateUserInput!) {
+  createUser(input: $input) {
+    id
+    username
+    lightOn
+    guestroomOpen
+  }
+}
+`;
+export const updateUser = `mutation UpdateUser($input: UpdateUserInput!) {
+  updateUser(input: $input) {
+    id
+    username
+    lightOn
+    guestroomOpen
+  }
+}
+`;
+export const deleteUser = `mutation DeleteUser($input: DeleteUserInput!) {
+  deleteUser(input: $input) {
+    id
+    username
+    lightOn
+    guestroomOpen
+  }
+}
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateUser = `subscription OnCreateUser {
+  onCreateUser {
+    id
+    username
+    lightOn
+    guestroomOpen
+  }
+}
+`;
+export const onUpdateUser = `subscription OnUpdateUser {
+  onUpdateUser {
+    id
+    username
+    lightOn
+    guestroomOpen
+  }
+}
+`;
+export const onDeleteUser = `subscription OnDeleteUser {
+  onDeleteUser {
+    id
+    username
+    lightOn
+    guestroomOpen
+  }
+}
+`;
